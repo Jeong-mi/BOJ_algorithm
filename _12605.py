0a2001a4cb5a1e460f034858f70d8e87d9b6ba42
jeong-mi <wjdalwjdal6951@gmail.com>
2022-01-29 12:55:56 +0900

    불필요한 주석제거

--- _12605.py
+++ _12605.py
@@ -1,7 +1,6 @@
 num = int(input())
 
-# IndexError: list assignment index out of range 
-# 위 같은 오류가 안생기도록 num개의 인덱스를 초기화해줌
+
 case =[]
 for i in range(num):
   words = input().split(' ') #['this', 'is', 'test]
