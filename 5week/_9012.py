3663d08561404ba4ad63f3c0f2bc86f3bfdb2e61
jeong-mi <wjdal6951@naver.com>
2022-02-25 00:06:04 +0900

    9012

--- 5week/_9012.py
+++ 5week/_9012.py
@@ -1,28 +1,27 @@
-#아직 못 품!!
 # ( 개수와 )개수가 같다고 YES가 나오진 않는다..
 import sys
 
 n = int(sys.stdin.readline())
 
-case = {'(': 0, ')': 0}
+sum = 0
 result = []
 
 for i in range(n):
   data = list(sys.stdin.readline().strip())
 
   for t in data:
-    if(t == '('):
-      case['('] += 1
-    else:
-      case[')'] += 1
-
-  if(case['('] == case[')']):
+    if(t == '('):#여는 괄호일 땐 sum +1
+      sum += 1
+    elif(t == ')'):#닫는 괄호일 땐 sum -1
+      sum -= 1
+      if(sum < 0): #하나씩 검사하면서 닫는 괄호가 더 많아졌다면
+        break      #닫는 괄호가 더 많아졌다면 괄호 한 쌍이 될 기회가 이젠 없기 때문에
+    
+  if(sum == 0):
     result.append('YES')
   else:
     result.append('NO')
-
-  case[')'] = 0
-  case['('] = 0
+  sum = 0
 
 for i in range(n):
   print(result[i])
